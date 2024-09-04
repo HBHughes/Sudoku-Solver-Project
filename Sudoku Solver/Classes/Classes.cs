a8c67da2a8b8fffb6d22995fe3eb751faaae510d
HBHughes <holdenhughes88@gmail.com>
2024-09-04 14:46:33 -0400

    Add project files.

---
+++ Sudoku Solver/Classes/Classes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku_Solver.Classes
+{
+    internal class Classes
+    {
+        public static bool UserValidInput(string k) 
+        {
+            try
+            {
+              int v = Convert.ToInt16(k);
+              if (v==0 || v==1)
+                {
+                    return true;
+                }
+              else
+                {
+                    return false;
+                }
+            }
+            catch {return false;}
+        }
+    }
+}
---
+++ Sudoku Solver/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Sudoku_Solver.Classes;
+bool Start = false;
+Console.WriteLine("Input Value for Operation\n[0] Input Sudoku \n[1] Generate Sudoku");
+while (Start == false)
+{
+    var userinput = Console.ReadLine();
+    if (userinput != null) 
+    { bool v = Classes.UserValidInput(userinput); 
+        if (v==true)
+        {
+            Start = true;
+        }
+        else
+        {
+            Console.WriteLine("No");  
+        }
+    }
+}
