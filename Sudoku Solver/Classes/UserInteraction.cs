63037f48c057f5e29c337f3e054fdf50a6d28830
HBHughes <holdenhughes88@gmail.com>
2024-09-06 13:04:17 -0400

    holy moly guacamole

--- Sudoku Solver/Classes/UserInteraction.cs
+++ Sudoku Solver/Classes/UserInteraction.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Sudoku_Solver.Classes
 {
     internal class UI
     {
-        public static bool UserValidInput(string k) 
+        public static bool UserValidInput(string? k) 
         {
             try
             {
@@ -24,11 +26,21 @@ namespace Sudoku_Solver.Classes
             }
             catch {return false;}
         }
-        public static void UserValidSudokuInput(string k)
+        public static bool UserValidSudokuInput(string? k)
         {
-            // 1. Check if String is Not Null and is 81 Characters and all Valid Integers
+            if (k != null && k.Length == 81 && AllCharAreInt(k)) // Checks if String is Not Null and is 81 Characters
+            {
+                return true;
+            }
             // 2. Return True/False -> True: Transfer to GridToArray False: Invalid Input
+            else { return false; }
         }
+        public static bool AllCharAreInt(string k)
+        {
+            var search = @"^-?[0-9]+(?:\.[0-9]+)?$";
+            var regex = new Regex(search);
+            return regex.IsMatch(k);
+        }   
     }
     internal class Sudoku
     {
@@ -46,13 +58,34 @@ namespace Sudoku_Solver.Classes
             return k;
             // Generates Blank Grid (81 0's) (String)
         }
-        public static void GridToArray(string k)
+        public static int[,] GridToArray(string k)
         {
-            for (int i = 0; i < k.Length; i++)
+            int[,] result = new int[9, 9];
+
+            for (int i = 0; i < 81; i++)
             {
-                // Splicing 1 Character at a time
+                // Calculate the row and column indices
+                int row = i / 9;
+                int col = i % 9;
+
+                // Convert character to integer and assign to the result array
+                result[row, col] = k[i] - '0';
             }
-            // Return Array
+
+            return result;
+        }
+        public static void PrintBoard(int[,] board)
+        {
+            int Sizerow = board.GetLength(0); //im adding this incase I want to add grids with other sizes (might not be possible need to work on algorithm)
+            int Sizecol = board.GetLength(1); 
+            for (int i = 0; i < Sizerow; i++)
+            {
+                for (int j = 0; j < Sizecol; j++)
+                {
+                    Console.Write(board[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
--- Sudoku Solver/Program.cs
+++ Sudoku Solver/Program.cs
@@ -1,25 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Sudoku_Solver.Classes;
 bool Start = false;
-int ProcessIdentifier = 100;
+int ProcessIdentifier = 1000;
 Console.WriteLine("Input Value for Operation\n[0] Input Sudoku \n[1] Generate Sudoku");
-while (Start == false) // Force User to Select A Process
+while (Start == false) // Force User to Select A Process note: There may be a more efficient console application but I'm going to leave this until I implement this into a website.
 {
-    var userinput = Console.ReadLine();
-    if (userinput != null) 
-    { bool v = UI.UserValidInput(userinput); 
-        if (v==true)
-        {
-            Start = true;
-            ProcessIdentifier = Convert.ToInt16(userinput);
-        }
-        else
-        {
-            Console.WriteLine("Invalid User Input, Try Again");
-        }
+    string? userinput = Console.ReadLine();
+    if (UI.UserValidInput(userinput) == true)
+    {
+        Start = true;
+        ProcessIdentifier = Convert.ToInt16(userinput);
+    }
+    else
+    {
+        Console.WriteLine("Invalid User Input, Try Again");
     }
 }
 if (Start == true && ProcessIdentifier == 0)
 {
-    Console.WriteLine(ProcessIdentifier);
+    Console.WriteLine("Input Valid Sudoku Grid:");
+    string? k = Console.ReadLine();
+    if (UI.UserValidSudokuInput(k) == true && k != null)
+    {
+        int[,] u = Sudoku.GridToArray(k);
+        for (int row = 0; row < 9; row++) //Prints my 2d Array
+        {
+            for (int col = 0; col < 9; col++)
+            {
+                Console.Write(u[row, col] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+    else { Console.WriteLine("Invalid User Input"); }
 }
