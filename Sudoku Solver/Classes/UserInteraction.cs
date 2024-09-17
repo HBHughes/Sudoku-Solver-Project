6cae8701e7b178c09f134322a9a1fa80ade27a20
HBHughes <holdenhughes88@gmail.com>
2024-09-17 14:11:31 -0400

    Big Changes, Need to Fix Generator

--- Sudoku Solver/Classes/UserInteraction.cs
+++ Sudoku Solver/Classes/UserInteraction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,7 +16,7 @@ namespace Sudoku_Solver.Classes
             try
             {
               int v = Convert.ToInt32(k);
-              if (v==0 || v==1 || v==2)
+              if (v==0 || v==1 || v==2 || v==3 || v==4) //ugly might look for alternative if I keep expanding operations
                 {
                     return true;
                 }
@@ -44,27 +45,27 @@ namespace Sudoku_Solver.Classes
     }
     internal class Sudoku
     {
-        private const int Size = 9;
+        private const int Size = 9; //no real reason to do size versus 9 but i think it adds readability instead of 9s everywhere
         private int[,] board = new int[Size, Size];
-        private Random rand = new Random();
-        public static bool Solve(int[,] board) // Backtracking & Method From https://norvig.com/sudoku.html adapted to C#
+        private Random rand = new Random(); //probably can be declared on a diff scope
+        public static bool Solve(int[,] board) // Backtracking & Method mostly From https://norvig.com/sudoku.html adapted to C#
         {
-            for (int row = 0; row < 9; row++)
+            for (int row = 0; row < Size; row++) //iterate row
             {
-                for (int col = 0; col < 9; col++)
+                for (int col = 0; col < Size; col++) //iteratre col
                 {
                     if (board[row, col] == 0) // Find an empty cell
                     {
-                        for (int num = 1; num <= 9; num++) // Try possible numbers
+                        for (int num = 1; num <= Size; num++) // Try possible numbers
                         {
                             if (IsValidNum(board, row, col, num))
                             {
                                 board[row, col] = num; // Place the number
-                                if (Solve(board)) // Recursively attempt to solve
+                                if (Solve(board)) // if Solve is true then return true feels counterintuitive but works
                                 {
                                     return true;
                                 }
-                                board[row, col] = 0; // Reset if not solved
+                                board[row, col] = 0; // Reset tile if not solved
                             }
                         }
                         return false; // Return false if no number is valid
@@ -73,10 +74,10 @@ namespace Sudoku_Solver.Classes
             }
             return true; // Solution found
         }
-        private static bool IsValidNum(int[,] board, int row, int col, int num)
+        public static bool IsValidNum(int[,] board, int row, int col, int num)
         {
             // Check if num is not in the current row
-            for (int c = 0; c < 9; c++)
+            for (int c = 0; c < Size; c++)
             {
                 if (board[row, c] == num)
                 {
@@ -85,7 +86,7 @@ namespace Sudoku_Solver.Classes
             }
 
             // Check if num is not in the current column
-            for (int r = 0; r < 9; r++)
+            for (int r = 0; r < Size; r++)
             {
                 if (board[r, col] == num)
                 {
@@ -151,7 +152,6 @@ namespace Sudoku_Solver.Classes
         private void RemoveNumbers()
         {
             int numberOfCellsToRemove = 30; // Adjust the number of cells to remove to create the puzzle difficulty - might have to look into some puzzle theory to improve this
-            // might need overload with extra input for cell count
             while (numberOfCellsToRemove > 0) //iterative to randomly remove 40 cells (might be inefficient hitting cells multiple times - will have to run tests)
             {
                 int row = rand.Next(Size);
@@ -168,21 +168,15 @@ namespace Sudoku_Solver.Classes
         {
             GenerateBoard();
         }
-        public static string BlankGrid()
-        {
-            string k = "000000000000000000000000000000000000000000000000000000000000000000000000000000000";
-            return k;
-            // Generates Blank Grid (81 0's) (String)
-        }
         public static int[,] GridToArray(string k)
         {
-            int[,] result = new int[9, 9];
+            int[,] result = new int[Size, Size];
 
             for (int i = 0; i < 81; i++)
             {
                 // Calculate the row and column indices
-                int row = i / 9;
-                int col = i % 9;
+                int row = i / Size;
+                int col = i % Size;
 
                 // Convert character to integer and assign to the result array
                 result[row, col] = k[i] - '0';
